4fac1c64badaf53f870a8c35ec7b30901bc50b5f
Forrest Aggar <faggar@et-k6.kzn.21-school.ru>
2021-10-29 14:38:56 +0300

    FIXEDPIPES

--- srcs/pipe3.c
+++ srcs/pipe3.c
@@ -20,6 +20,9 @@ void	some_inits(t_data *data)
 	data->commands[1][0] = "cat";
 	data->commands[1][1] = "-e";
 	data->commands[1][2] = NULL;
+	// data->commands[2][0] = "cat";
+	// data->commands[2][1] = "-e";
+	// data->commands[2][2] = NULL;
 	data->commands[2][0] = "pwd";
 	data->commands[2][1] = NULL;
 	data->commands[2][2] = NULL;
@@ -54,6 +57,7 @@ void	ft_new_pipe(t_data *data, char **env)
 	fill_env_path(data, env);
 	some_inits(data);
 	int	p[2][2];
+
 	pipe(p[0]);
 	pipe(p[1]);
 	pidt	pid[2];
@@ -61,12 +65,15 @@ void	ft_new_pipe(t_data *data, char **env)
 	char *cmd;
 	int		pipes;
 
-	pipes = 2 + 1;
-	while(i < pipes)
+	pipes = 2;
+	while(i <= pipes)
 	{
 		cmd = ft_strdup(find_path1(data, i));
 		pid[i].pid = fork();
-
+		if (pid[i].pid > 0)
+		{
+			close(p[i][1]);
+		}
 		if (pid[i].pid == 0)
 		{	
 			if (pid[i].pid == 0 && i == 0)
@@ -74,25 +81,35 @@ void	ft_new_pipe(t_data *data, char **env)
 				close(p[i][0]);
 				dup2(p[i][1], 1);
 			}
-			else if (i < pipes)//условие может быть ненужным
+			else if (i != pipes)
 			{
 				close(p[i - 1][1]);
 				dup2(p[i - 1][0], 0);
 				close(p[i][0]);
 				dup2(p[i][1], 1);
-
 			}
-			else
+			else if (i == pipes)
 			{
-				close(p[i][1]);
-				dup2(p[i][0], 0);
+				close(p[i - 1][1]);
+				dup2(p[i - 1][0], 0);
+				
+				execve(cmd, data->commands[i], env);
 			}
 			if ((execve(cmd, data->commands[i], env)) == -1)
 				kill(pid[i].pid, SIGTERM);
 		}
 		i++;
 	}
+// int ier = 0;
+
+// while (ier < 3)
+// {
+// 	wait(0);
+// 	ier++;
+// }
+// while(1);
 }
+
 
 int main (int ac, char **av, char **env)
 {
@@ -101,42 +118,3 @@ int main (int ac, char **av, char **env)
 	ft_new_pipe(&data, env);
 }
 
-
-
-// void	ft_new_pipe(t_data *data, char **env)
-// {
-// 	fill_env_path(data, env);
-// 	some_inits(data);
-// 	int	p[2][2];
-// 	pipe(p[0]);
-// 	pipe(p[1]);
-// 	pidt	pid[2];
-// 	int	i = 0;
-// 	char *cmd;
-// 	while(i < 3)
-// 	{
-// 		cmd = ft_strdup(find_path1(data, i));
-// 		pid[i].pid = fork();
-
-// 		if (pid[i].pid == 0 && i == 0)
-// 		{
-// 			close(p[i][0]);
-// 			dup2(p[i][1], 1);
-// 			execve(cmd, data->commands[i], env);
-// 		}
-// 		else if (pid[i].pid == 0 && (i == 0 || i == 1) && i != 3 )
-// 		{
-// 			dup2(p[i][0], 0);
-// 			dup2(p[i][1], 1);
-// 			execve(cmd, data->commands[i], env);
-// 		}
-// 		else if (pid[i].pid == 0)
-// 		{
-// 			close(p[i][1]);
-// 			dup2(p[i][0], 0);
-// 			execve(cmd, data->commands[i], env);
-// 		}
-// 		i++;
-// 	}
-// }
-//нужно двумерный массив превратить в единицу и тройку на 2 заменить
