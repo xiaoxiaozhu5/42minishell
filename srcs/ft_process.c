ee66fbf425cdbb0cad5762457e683f40924b0ba6
AdmiralXy <gameproblem80@gmail.com>
2021-11-08 14:32:15 +0300

    fix process

--- srcs/ft_process.c
+++ srcs/ft_process.c
@@ -3,8 +3,8 @@
 void	ft_process(t_env *env)
 {
 	(void)env;
-	if (env->cmds->command && !ft_strncmp(env->cmds->command, "export", ft_strlen(env->cmds->command)))
+	if (env->cmds->command && !ft_strcmp(env->cmds->command, "export"))
 		ft_export(env->cmds, env);
-	if (env->cmds->command && !ft_strncmp(env->cmds->command, "unset", ft_strlen(env->cmds->command)))
+	if (env->cmds->command && !ft_strcmp(env->cmds->command, "unset"))
 		ft_unset(env->cmds, env);
 }
