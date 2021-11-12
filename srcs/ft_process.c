54a71bd7d4cdb0c3a35b809a805ef4981f65fc9a
AdmiralXy <gameproblem80@gmail.com>
2021-11-12 12:58:51 +0300

    update signals

--- srcs/ft_process.c
+++ srcs/ft_process.c
@@ -7,4 +7,8 @@ void	ft_process(t_env *env)
 		ft_export(env->cmds, env);
 	if (env->cmds->command && !ft_strcmp(env->cmds->command, "unset"))
 		ft_unset(env->cmds, env);
+	if (env->cmds->command && !ft_strcmp(env->cmds->command, "pwd"))
+		ft_pwd(env);
+	if (env->cmds->command && !ft_strcmp(env->cmds->command, "cd"))
+		ft_cd(env->cmds, env);
 }
