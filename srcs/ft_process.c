0629ff228eb10d58dd66e46f78b4088cae4b7a45
AdmiralXy <gameproblem80@gmail.com>
2021-11-16 16:07:04 +0300

    add execve

--- srcs/ft_process.c
+++ srcs/ft_process.c
@@ -2,25 +2,6 @@
 
 void	ft_process(t_env *env)
 {
-	(void)env;
-	if (env->cmds->command && !ft_strcmp(env->cmds->command, "export"))
-		ft_export(env->cmds, env);
-	if (env->cmds->command && !ft_strcmp(env->cmds->command, "unset"))
-		ft_unset(env->cmds, env);
-	if (env->cmds->command && !ft_strcmp(env->cmds->command, "pwd"))
-		ft_pwd(env);
-	if (env->cmds->command && !ft_strcmp(env->cmds->command, "cd"))
-		ft_cd(env->cmds, env);
-	if (env->cmds->command && !ft_strcmp(env->cmds->command, "echo"))
-		ft_echo(env->cmds, env);
-	if (env->cmds->command && !ft_strcmp(env->cmds->command, "env"))
-		ft_env(env);
-	if (env->cmds->command && !ft_strcmp(env->cmds->command, "exit"))
-		ft_exit(env);
-
-	// printf("n pipes %d\n", env->n_pipes);
-
-
 	// if (env->cmds->command != NULL && env->n_pipes > 0)
 		ft_new_pipe(env);
 
