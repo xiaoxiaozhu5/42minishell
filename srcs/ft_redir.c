ff6cb439b22538b8aa52b8a1bc3cafc5ea6e9a68
AdmiralXy <gameproblem80@gmail.com>
2021-11-26 16:56:52 +0300

    working on signals

--- srcs/ft_redir.c
+++ srcs/ft_redir.c
@@ -1,18 +1,4 @@
-#include "../includes/ft_minishell.h"
-
-// void	sigint(int signal)
-// {
-// 	(void)signal;
-// 	exit(130);
-// }
-
-// void	sigquit(int signal)
-// {
-// 	(void)signal;
-// 	rl_on_new_line();
-// 	rl_redisplay();
-// 	exit(130);
-// }
+#include "ft_minishell.h"
 
 static int	double_left(t_redir *redir, t_node *cmd_info)
 {
